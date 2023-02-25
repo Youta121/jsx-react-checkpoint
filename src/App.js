07d8f32203fb45a3fa991e6d1f4787f8d8c558f2
eya <eya100tounsi@gmail.com>
2023-02-25 19:36:10 +0100

    add rep

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Adress from "./Adress" ;
+import Name from "./Name" ;
+import Photo from "./Photo" ;
+import React from "react";
+
+//import MyFirstComponent from "./MyFirstComponent";
+const App = () => (
+  <>
+    <Name />
+    <Adress />
+    <Photo />
+
+  </>
+ );
+ export default App;
+
+
+
+
+
+
+
+  
+    
